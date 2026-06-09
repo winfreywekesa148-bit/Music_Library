95c265cdc4063d76a2da889cb2db179ed432a5cc
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-06-09 20:48:13 +0300

    call methods implemented

--- lib/song.py
+++ lib/song.py
@@ -18,22 +18,23 @@ class Song:
     @classmethod
     def add_song_to_count(cls, count):
         return count + 1 #class method
-        
-        
-
+    
     def add_to_genres(cls, genre):
-        if genre is not  cls.genre:
+        if genre is not cls.genre:
             cls.genre.append(genre)
         return Song.genre_count + 1
         
 
-    def add_to_artists(cls):
-        pass
+    def add_to_artists(cls, artist):
+        if artist is not cls.artist:
+            cls.artist.append(artist)
+            return Song.artist_count + 1
+        
 
     def add_to_genre_count(cls):
-        pass
+        return Song.genre_count + 1
 
     def add_to_artists_count(cls):
-        pass
+        return Song.artist_count + 1
 
     
