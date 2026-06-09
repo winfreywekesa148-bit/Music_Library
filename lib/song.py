bdc32ad5ec666fd0aa3a93004fcc011ef37bbd9b
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-06-09 21:39:00 +0300

    Update song.py

--- lib/song.py
+++ lib/song.py
@@ -8,7 +8,7 @@ class Song:
     def __init__(self, name, artist, genre):
         self.name = name
         self.artist = artist
-        self.genre = genre
+        self.genre = [artist] if isinstance(artist, str) else list(artist)
         Song.count += 1
         Song.genres +=1
         Song.artists +=1
