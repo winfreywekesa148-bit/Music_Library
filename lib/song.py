80bcc8b706a8fd09b47cf8ce0bc10773027c9591
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-06-09 22:22:36 +0300

    change in the class method

--- lib/song.py
+++ lib/song.py
@@ -1,40 +1,24 @@
 class Song:
-    count = 0 #class atribute
-    genres = 0
-    artists = 0
-    genre_count = 0
-    artist_count = 0
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
 
     def __init__(self, name, artist, genre):
         self.name = name
         self.artist = artist
         self.genre = genre
+
         Song.count += 1
-        Song.genres = []
-        Song.artists = []
-        Song.genre_count = {}
-        Song.artist_count = {}
 
-    @classmethod
-    def add_song_to_count(cls, count):
-        return count + 1 #class method
-    
-    def add_to_genres(cls, genre):
-        if genre is not cls.genre:
-            cls.genre.append(genre)
-        return Song.genre_count + 1
-        
+        if genre not in Song.genres:
+            Song.genres.append(genre)
 
-    def add_to_artists(cls, artist):
-        if artist is not cls.artist:
-            cls.artist.append(artist)
-            return Song.artist_count + 1
-        
+        if artist not in Song.artists:
+            Song.artists.append(artist)
 
-    def add_to_genre_count(cls):
-        return Song.genre_count + 1
-
-    def add_to_artists_count(cls):
-        return Song.artist_count + 1
+        Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
+        Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
 
     
