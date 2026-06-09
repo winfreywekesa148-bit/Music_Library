ba582a92b21698c9b8da574d1057631f0f287ae6
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-06-09 21:31:25 +0300

    Fix logic for genre and artist checks

--- lib/song.py
+++ lib/song.py
@@ -11,22 +11,22 @@ class Song:
         self.genre = genre
         Song.count += 1
         Song.genres +=1
-        Song.artist +=1
+        Song.artists +=1
         Song.genre_count +=1
         Song.artist_count +=1
-    
+
     @classmethod
     def add_song_to_count(cls, count):
         return count + 1 #class method
     
     def add_to_genres(cls, genre):
-        if genre not in cls.genre:
+        if genre is not cls.genre:
             cls.genre.append(genre)
         return Song.genre_count + 1
         
 
     def add_to_artists(cls, artist):
-        if artist not in cls.artist:
+        if artist is not cls.artist:
             cls.artist.append(artist)
             return Song.artist_count + 1
         
