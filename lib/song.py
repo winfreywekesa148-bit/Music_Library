68609ceb1ba4e33bc037897be80c3cd62330ef7b
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-06-09 21:25:47 +0300

    Update Song class to manage genres and artists
    Refactor Song class to use lists and dictionaries for genres and artists tracking.

--- lib/song.py
+++ lib/song.py
@@ -1,32 +1,38 @@
 class Song:
     count = 0 #class atribute
-    genres = 0
-    artists = 0
-    genre_count = 0
-    artist_count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
 
     def __init__(self, name, artist, genre):
         self.name = name
         self.artist = artist
         self.genre = genre
+        
         Song.count += 1
-        Song.genres +=1
-        Song.artists +=1
-        Song.genre_count +=1
-        Song.artist_count +=1
+
+    if genre not in Song.genres:
+        Song.genres.append(genre)
+
+    if artist not in Song.artists:
+        Song.artists.append(artist)
+
+    Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
+    Song.artist_count[artist] = Song.artist_count.get(artist, 0) +1
 
     @classmethod
     def add_song_to_count(cls, count):
         return count + 1 #class method
     
     def add_to_genres(cls, genre):
-        if genre is not cls.genre:
+        if genre not in cls.genre:
             cls.genre.append(genre)
         return Song.genre_count + 1
         
 
     def add_to_artists(cls, artist):
-        if artist is not cls.artist:
+        if artist not is cls.artist:
             cls.artist.append(artist)
             return Song.artist_count + 1
         
