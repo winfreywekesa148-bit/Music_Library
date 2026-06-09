6e4694b9e1a7791364ac08fc3965fa05eb5dcf9c
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-06-09 17:52:10 +0300

    class method and class attribute add in the file

--- lib/song.py
+++ lib/song.py
@@ -1,1 +1,39 @@
+class Song:
+    count = 0 #class atribute
+    genres = 0
+    artist =0
+    genre_count = 0
+    artist_count = 0
 
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = []
+        Song.count += 1
+        Song.genres +=1
+        Song.artist +=1
+        Song.genre_count +=1
+        Song.artist_count +=1
+
+    @classmethod
+    def add_song_to_count(cls, count):
+        return count + 1 #class method
+        
+        
+
+    def add_to_genres(cls, genre):
+        if genre is not  cls.genre:
+            cls.genre.append(genre)
+        return Song.genre_count + 1
+        
+
+    def add_to_artists(cls):
+        pass
+
+    def add_to_genre_count(cls):
+        pass
+
+    def add_to_artists_count(cls):
+        pass
+
+    
